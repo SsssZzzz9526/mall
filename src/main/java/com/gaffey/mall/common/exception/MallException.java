726f8bd917d7974a3cce3cd403ae266c81ab172a
gaffey <gaffey@gmail.com>
2020-09-18 01:04:03 +0800

    feat(conf): 基础异常

---
+++ src/main/java/com/gaffey/mall/common/exception/MallException.java
@@ -0,0 +1,11 @@
+package com.gaffey.mall.common.exception;
+
+/**
+ * mall项目基础异常
+ *
+ * @author gaffey
+ * @created 2020-09-17 22:21:00
+ */
+public class MallException extends RuntimeException {
+
+}
